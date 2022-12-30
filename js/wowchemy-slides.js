fcbbb496637fa46c588d3e89f69bb359ae9b501c
Philip-Valentino <philipduah1992@gmail.com>
2022-12-30 06:21:05 +0100

    recent update

--- js/wowchemy-slides.js
+++ js/wowchemy-slides.js
@@ -2,7 +2,7 @@
   // ns-params:@params
   var slides = { highlight_style: "dracula", theme: "black" };
 
-  // ns-hugo:W:\webs\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5\assets\js\wowchemy-utils.js
+  // ns-hugo:E:\school\TUBAF DOCUMENTS\WEBSITE\REAL WEBSITE_UPDATED_2022\webs\themes\github.com\wowchemy\wowchemy-hugo-modules\wowchemy\v5\assets\js\wowchemy-utils.js
   function fixMermaid(render = false) {
     let mermaids = [];
     [].push.apply(mermaids, document.getElementsByClassName("language-mermaid"));
